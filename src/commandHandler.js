f998b905461b491e10576da4ce09c5c5f7d57c66
chandra bachtiar <chandrabahtiar25@gmail.com>
2022-09-18 00:33:22 +0700

    [Complete] Image to sticker with config

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -3,25 +3,50 @@ const { chatType } = require("./chatType");
 const { downloadMediaMessage } = require("@adiwajshing/baileys");
 
 async function commandHandler(chat, client, message, command) {
-    // const msgType = await chatType(chat);
-    // if(msgType.media) {
-    //     console.log(msgType);
-    // }
-    const didix = chat.key.remoteJid.replace("@s.whatsapp.net", "");
-    if (didix === "6285221913659") {
-        console.log(chat);
-        const mediaData = await downloadMediaMessage(chat, "buffer", {});
-        const stickerOptions = {
-            pack: "Ocan-bot",
-            author: "+62 859-1066-03535",
-            type: StickerTypes.CROPPED,
-            categories: ['🤩', '🎉'],
-            quality: 50,
-        };
-        const generateSticker = await createSticker(mediaData, stickerOptions);
-        await client.sendMessage(chat.key.remoteJid, {
-            sticker: generateSticker,
-        });
+    const msgType = await chatType(chat);
+    console.log(msgType);
+    const from = chat.key.remoteJid || '';
+
+
+    if(from === '') {
+        console.log(`[OCAN] Oops failed get number from!`);
+        return
+    }
+
+    //STICKER
+    if(msgType.valid) {
+        if(msgType.type === "image" || msgType.type === "video") {
+            let caption = Object.values(chat.message)[0].caption || '';
+            let stickerType;
+            switch(true) {
+                case caption.toLowerCase().includes("crop"):
+                    stickerType = StickerTypes.CROPPED;
+                    break;
+                case caption.toLowerCase().includes("full"):
+                    stickerType = StickerTypes.FULL;
+                    break;
+                case caption.toLowerCase().includes("bulat"):
+                    stickerType = StickerTypes.CIRCLE;
+                    break;
+                default:
+                    stickerType = StickerTypes.DEFAULT;
+                    break;
+            }
+
+            const stickerOptions = {
+                pack: "Ocan-bot",
+                author: "+62 859-1066-03535",
+                type: stickerType,
+                categories: ['🤩', '🎉'],
+                quality: 50,
+            };
+
+            const mediaData = await downloadMediaMessage(chat, "buffer", {});
+            const generateSticker = await createSticker(mediaData, stickerOptions);
+            await client.sendMessage(chat.key.remoteJid, {
+                sticker: generateSticker,
+            });
+        }
     }
 }
 
